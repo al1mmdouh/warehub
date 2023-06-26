b2c673d2fbcf266577991dbdd7086883300347e6
al1mmdouh <alimmdouh340@gmail.com>
2023-06-26 18:54:14 +0300

    Automatic hash passwords

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -43,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+    
 }
