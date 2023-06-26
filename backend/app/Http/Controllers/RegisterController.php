0ddc75fb57a757cab177769d9cd38f81e73f092e
al1mmdouh <alimmdouh340@gmail.com>
2023-06-26 19:02:46 +0300

    Edit validation for password

--- backend/app/Http/Controllers/RegisterController.php
+++ backend/app/Http/Controllers/RegisterController.php
@@ -12,7 +12,7 @@ class RegisterController extends Controller
         $attributes = request()->validate([
             'name'=>'required|max:255',
             'email'=>'required|email|max:255',
-            'password'=>'required|max:255|min:7',
+            'password'=>'required|min:7|max:255',
             'phone_number' => 'required|max:255',
             'address' => 'required|max:255',
         ]);
