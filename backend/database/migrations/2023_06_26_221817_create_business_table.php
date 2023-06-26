384e0389cb9551c3d899fd86e2cf9426b233b0ea
al1mmdouh <alimmdouh340@gmail.com>
2023-06-27 01:18:45 +0300

    Create migration for business

---
+++ backend/database/migrations/2023_06_26_221817_create_business_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('business_name', 255);
+            $table->string('business_address', 255);
+            $table->string('business_type', 255);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business');
+    }
+};
