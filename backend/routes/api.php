2ccfc2cf74bde4413a7198ce58d5fda1a5a0a0d6
al1mmdouh <alimmdouh340@gmail.com>
2023-06-27 00:45:41 +0300

    Added route for login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register',[RegisterController::class,'store'])->middleware('guest');
+
+Route::post('login',[SessionController::class,'store'])->middleware('guest');
