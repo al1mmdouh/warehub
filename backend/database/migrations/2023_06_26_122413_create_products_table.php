40b14a18a5ac00b8e5c93b9e46e26d264aa9e0d2
Ahmed Abobasha <ahmedabobasah10@gmail.com>
2023-06-26 20:44:11 +0300

    create migration for products and orders and list mile company

---
+++ backend/database/migrations/2023_06_26_122413_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('merchant_id');
+            $table->foreign('merchant_id')->references('id')->on('merchants')->onDelete('cascade');
+            $table->unsignedBigInteger('warehouse_id');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('sku');
+            $table->decimal('price',10,2);
+            $table->double('weight',10,3)->default(0);
+            $table->text('image')->nullable();
+            $table->unique(['sku','merchant_id']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
