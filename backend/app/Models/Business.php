69b740a9251d4e5dff12b276c29464ac2cb7d800
al1mmdouh <alimmdouh340@gmail.com>
2023-06-27 01:21:43 +0300

    Created Business model with fillable

---
+++ backend/app/Models/Business.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Business extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'business_name',
+        'business_address',
+        'business_type',
+    ];
+
+
+}
