a83510ff6dbc68e08903eacee89e47c87de731e4
al1mmdouh <alimmdouh340@gmail.com>
2023-06-26 23:23:21 +0300

    Created migration for role column in user table

---
+++ backend/database/migrations/2023_06_26_195133_add_role_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', [0, 1])->default(0); // 0 business owner, 1 admin
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
