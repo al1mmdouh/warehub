a6b2a815e512642883f76f68dc82e618ccf251d3
al1mmdouh <alimmdouh340@gmail.com>
2023-06-26 18:59:11 +0300

    add store function with validation

--- backend/app/Http/Controllers/RegisterController.php
+++ backend/app/Http/Controllers/RegisterController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function store() {
+
+        $attributes = request()->validate([
+            'name'=>'required|max:255',
+            'email'=>'required|email|max:255',
+            'password'=>'required|max:255|min:7',
+            'phone_number' => 'required|max:255',
+            'address' => 'required|max:255',
+        ]);
+        
+        User::create($attributes);
+
+        return redirect('/'); //redirect to home page
+    }
 }
