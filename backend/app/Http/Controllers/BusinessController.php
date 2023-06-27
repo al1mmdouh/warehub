9f904366fb77b0568e3be9b307f21381dde995ce
al1mmdouh <alimmdouh340@gmail.com>
2023-06-27 15:11:54 +0300

    Created business controller with validation and store

---
+++ backend/app/Http/Controllers/BusinessController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Business;
+use App\Models\User;
+
+class BusinessController extends Controller
+{
+    public function store()
+    {
+
+        $attributes = request()->validate([
+            'user_id' => 'required|exists:user,id',
+            'business_name' => 'required|min:3|max:255',
+            'business_address' => 'required',
+            'business_type' => 'required|in:product,warehouse',
+        ]);
+
+        $business = Business::create($attributes);
+
+        return response()->json($business, 201); //redirect to home page
+    }
+}
