720fc5be50a481e6c4f6175120820fcd2924c201
al1mmdouh <alimmdouh340@gmail.com>
2023-06-27 16:01:58 +0300

    Update Business controller, Check if validation fail

--- backend/app/Http/Controllers/BusinessController.php
+++ backend/app/Http/Controllers/BusinessController.php
@@ -4,20 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Business;
 use App\Models\User;
+use Validator;
 
 class BusinessController extends Controller
 {
     public function store()
     {
 
-        $attributes = request()->validate([
+        $validator = Validator::make(request()->all(),[
+            'user_id' => 'required|exists:users,id',
             'business_name' => 'required|min:3|max:255',
             'business_address' => 'required',
             'business_type' => 'required|in:product,warehouse',
         ]);
+        
+        if ($validator->fails()) {
+            return $this->respondValidationErrors($validator->errors());
+        }
 
-        $business = Business::create($attributes);
+        $business = Business::create($validator->validated());
 
-        return response()->json($business, 201); //redirect to home page
+        return response()->json($business, 201); 
     }
 }
