587d033d42cf0896cbeccc3bef544aacc1df296c
AhmadTaha24 <ahmadtaha2248@gmail.com>
2023-07-02 13:38:20 +0300

    fixng lazy loading

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WarehouseModule } from './warehouse/warehouse.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { WarehouseModule } from './warehouse/warehouse.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    //WarehouseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
