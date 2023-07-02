c344d4316c5f12752d9b03d4afc5d5d36dc62cc8
AhmadTaha24 <ahmadtaha2248@gmail.com>
2023-07-03 01:02:55 +0300

    latest working

--- frontend/src/app/warehouse/warehouse-register/warehouse-register.component.ts
+++ frontend/src/app/warehouse/warehouse-register/warehouse-register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-warehouse-register',
@@ -8,17 +8,39 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class WarehouseRegisterComponent {
   myForm !: FormGroup;
+  countForm !: FormGroup;
 
+
+  warehouseNumber: number = 1;
   constructor(private fb: FormBuilder){}
 
   ngOnInit(){
     this.myForm = this.fb.group({
-      name: '',
-      warehouseType: '',
-      warehouseAddress: '',
-      capacity: '',
-      serviceFeePerVolume: '',
-      shipments: Boolean
+      name: ['',[Validators.minLength(3),  Validators.required]],
+      warehouseType: ['',[ Validators.minLength(5), Validators.required ]],
+      warehouseAddress: ['',[Validators.minLength(5), Validators.required ]],
+      capacity: ['',[ Validators.min(1), Validators.required ]],
+      serviceFeePerVolume: ['',[Validators.min(1), Validators.required ]],
+      shipments: [true]
     })
+
+    this.countForm = this.fb.group({
+      count: ['', [Validators.min(0), Validators.max(5), Validators.required]],
+    })
+    
+  }
+
+  changeWarehouse(number: number){
+    this.warehouseNumber = number;
+  }
+
+  onSubmitCount(){
+    if(this.countForm.valid){
+      this.warehouseNumber= parseInt(this.countForm.value.count)
+    }
+
+  }
+  onSubmitForm(){
+    console.log(this.myForm.value.shipments);
   }
 }
